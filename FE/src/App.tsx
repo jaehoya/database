f582ec0984da16bb41a852ed510974ce08da4681
jaehoya <hmujaeho@gmail.com>
2025-11-25 14:25:35 +0900

    init

---
+++ FE/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+//page routing
+import Home from './pages/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
